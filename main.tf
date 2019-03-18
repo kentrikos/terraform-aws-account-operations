fbf96105d770b1e54787f4629fa5fe50029b0d72
Radosław Palczyński <radoslaw.palczynski@nordcloud.com>
2019-03-18 13:35:52 +0100

    Test eks 0.2.2

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ locals {
 
 # Kubernetes cluster:
 module "kubernetes_cluster_operations" {
-  source = "github.com/kentrikos/terraform-aws-eks?ref=0.2.2"
+  source = "github.com/kentrikos/terraform-aws-eks?ref=feature/enable_helm"
 
   cluster_prefix            = "${local.cluster_name}"
   region                    = "${var.region}"
