c37fb65c5617ee1f02c60e6b9e4ed110778f9026
Michael Greer <michael.greer@bmwgroup.com>
2019-04-12 10:37:27 -0400

    added outputs

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ locals {
 
 # Kubernetes cluster:
 module "kubernetes_cluster_operations" {
-  source = "github.com/kentrikos/terraform-aws-eks?ref=0.2.2"
+  source = "github.com/kentrikos/terraform-aws-eks?ref=1.1.0"
 
   cluster_prefix                = "${local.cluster_name}"
   region                        = "${var.region}"
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
+output "cluster_id" {
+  value = "${module.kubernetes_cluster_operations.cluster_id}"
+}
 
+output "worker_iam_role_name" {
+  value = "${module.kubernetes_cluster_operations.worker_iam_role_name}"
+}
